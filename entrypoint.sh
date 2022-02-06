d43c006eabe5cbccad6a5adf1f0599858e1e1be4
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2022-02-06 14:03:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 #SSH Key Vars 
 SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
-SSH_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
+SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
 
 
 ###
@@ -21,35 +21,35 @@ SSH_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
 #     export WPE_ENV_NAME=${INPUT_NEW_ENV_NAME};    
 ###
 
-if [[ $GITHUB_REF =~ ${INPUT_BRANCH}$ ]]; then
-    export ENV_NAME=$INPUT_ENVIROMENT; 
+if [[ $GITHUB_REF =~ ${INPUT_PRD_BRANCH}$ ]]; then
+    export WPE_ENV_NAME=$INPUT_PRD_ENV;
 else 
     echo "FAILURE: Branch name required." && exit 1;
 fi
 
-echo "Deploying $GITHUB_REF to $ENV_NAME..."
+echo "Deploying $GITHUB_REF to $WPE_ENV_NAME..."
 
 #Deploy Vars
-REMOTE_SERVER="162.241.194.20"
-REMOTE_DIR_PATH="deploy-test"
+WPE_SSH_HOST="162.241.194.20"
+DIR_PATH="deploy-test"
 SRC_PATH="."
  
 # Set up our user and path
 
-SSH_USER="olehrusyi"@"$REMOTE_SERVER"
-DESTINATION=$SSH_USER":"$REMOTE_DIR_PATH
+WPE_SSH_USER="olehrusyi"@"$WPE_SSH_HOST"
+WPE_DESTINATION=$WPE_SSH_USER":"$DIR_PATH
 
 # Setup our SSH Connection & use keys
 mkdir "$SSH_PATH"
-ssh-keyscan -t rsa "$REMOTE_SERVER" >> "$KNOWN_HOSTS_PATH"
+ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 
 #Copy Secret Keys to container
-echo "$INPUT_SSHG_KEY_PRIVATE" > "$SSH_KEY_PRIVATE_PATH"
+echo "$INPUT_SSH_KEY_PRIVATE" > "$SSHG_KEY_PRIVATE_PATH"
 #Set Key Perms 
 chmod 700 "$SSH_PATH"
 chmod 644 "$KNOWN_HOSTS_PATH"
-chmod 600 "$SSH_KEY_PRIVATE_PATH"
+chmod 600 "$SSHG_KEY_PRIVATE_PATH"
 
 # Deploy via SSH
 # Exclude restricted paths from exclude.txt
-rsync --rsh="ssh -v -p 2222 -i ${SSH_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" $INPUT_FLAGS --exclude-from='/exclude.txt' $SRC_PATH "$DESTINATION"
+rsync --rsh="ssh -v -p 2222 -i ${SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" $INPUT_FLAGS --exclude-from='/exclude.txt' $SRC_PATH "$WPE_DESTINATION"
